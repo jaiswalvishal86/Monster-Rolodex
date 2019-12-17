9cca8e07cc6a5b439fae997740b18a0c8992ace5
Vishal Jaiswal <jaiswalvishal86@gmail.com>
2019-12-17 08:02:07 +0530

    Updates

---
+++ precache-manifest.7bd9c818dfe120e957f120eb87aa297d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1595b12ff1fcdf613db91476e13d955c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "54664f90a7ef320a3e8b",
+    "url": "/monsters-rolodex/static/css/main.d8e2568e.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "54664f90a7ef320a3e8b",
+    "url": "/monsters-rolodex/static/js/main.8f3e7466.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
